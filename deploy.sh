98f88fe8e6d1e3e978bd226444c8156a5c533979
ANDeaD-K <andead@inbox.ru>
2019-01-10 13:20:31 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ev
 
+echo "dotnet andead.netcore.oauth.dll --client-id={$CLIENT_ID} --client-secret={$CLIENT_SECRET} --redirect-uri={$REDIRECT_URI} --issuer={$ISSUER} --audience={$AUDIENCE} --signing-key={$SIGNING_KEY}" >> ./publish/entrypoint.sh
+
 docker build -t andead/netcore.oauth:latest publish/.
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
