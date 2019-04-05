50266c40073b9ed6f339ce898f139160863719a5
ANDeaD <andead@inbox.ru>
2019-04-05 22:49:48 +0300

    JWT Token expires in 1 day

--- Managers/JwtManager.cs
+++ Managers/JwtManager.cs
@@ -1,25 +1,34 @@
-
-
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using andead.netcore.oauth.Models;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 
 namespace andead.netcore.oauth.Managers
 {
     public class JwtManager
     {
         private readonly ConfigurationManager _configurationManager;
+        private readonly ILogger _logger;
 
-        public JwtManager(ConfigurationManager configurationManager)
+        public JwtManager(ConfigurationManager configurationManager, ILogger logger)
         {
             _configurationManager = configurationManager;
+            _logger = logger;
         }
 
         public string GenerateJwtToken(UsersGetResponse userInfo)
         {
+            _logger.LogInformation(JsonConvert.SerializeObject(
+                new
+                {
+                    info = $"Generate JWT Token for user {userInfo.response[0].first_name} ({userInfo.response[0].id})"
+                }
+            ));
+
             var claims = new List<Claim>()
             {
                 new Claim(ClaimTypes.NameIdentifier, userInfo.response[0].id.ToString()),
@@ -31,7 +40,7 @@ namespace andead.netcore.oauth.Managers
                 issuer: _configurationManager.GetValue(ConfigurationKey.ISSUER),
                 audience: _configurationManager.GetValue(ConfigurationKey.AUDIENCE),
                 claims: claims,
-                expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(30)),
+                expires: DateTime.UtcNow.Add(TimeSpan.FromDays(1)),
                 signingCredentials: new SigningCredentials(_configurationManager.GetSymmetricSecurityKey(ConfigurationKey.SIGNING_KEY), SecurityAlgorithms.HmacSha256)
             );
 
